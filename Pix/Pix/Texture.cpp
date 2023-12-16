6a088784a5bce5698f7a74d70e08c11a04dbf2f5
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-12-16 09:26:39 -0800

    Bilinear method

--- Pix/Pix/Texture.cpp
+++ Pix/Pix/Texture.cpp
@@ -3,39 +3,61 @@
 namespace
 {
 #pragma pack(push, 1)
-    struct BitmapFileHeader
-    {
-        uint16_t type;          // magic identifier
-        uint32_t size;          // file size in bytes
-        uint16_t reserved1;
-        uint16_t reserved2;
-        uint32_t offset;        // byte offset to image data
-    };
+	struct BitmapFileHeader
+	{
+		uint16_t type;          // magic identifier
+		uint32_t size;          // file size in bytes
+		uint16_t reserved1;
+		uint16_t reserved2;
+		uint32_t offset;        // byte offset to image data
+	};
 
-    struct BitmapInfoHeader
-    {
-        uint32_t size;                  // header size in bytes
-        int width, height;              // width and height of the image
-        uint16_t planes;                // number of color planes
-        uint16_t bits;                  // bits per pixel
-        uint32_t compression;           // compression type
-        uint32_t imageSize;             // image size in bytes
-        int xResolution, yResolution;   // pixels per meter
-        uint32_t numColors;             // number of colors
-        uint32_t importantColors;       // important colors
-    };
+	struct BitmapInfoHeader
+	{
+		uint32_t size;                  // header size in bytes
+		int width, height;              // width and height of the image
+		uint16_t planes;                // number of color planes
+		uint16_t bits;                  // bits per pixel
+		uint32_t compression;           // compression type
+		uint32_t imageSize;             // image size in bytes
+		int xResolution, yResolution;   // pixels per meter
+		uint32_t numColors;             // number of colors
+		uint32_t importantColors;       // important colors
+	};
 
-    uint32_t MakeStringAligned(uint32_t rowStride, uint32_t alignStride)
-    {
-        uint32_t newStride = rowStride;
-        while (newStride % alignStride != 0)
-        {
-            newStride++;
-        }
+	uint32_t MakeStringAligned(uint32_t rowStride, uint32_t alignStride)
+	{
+		uint32_t newStride = rowStride;
+		while (newStride % alignStride != 0)
+		{
+			newStride++;
+		}
 
-        return newStride;
-    }
+		return newStride;
+	}
 #pragma pack(pop)
+
+	X::Color GetBilinearFilterPixelColor(const Texture& texture, float u, float v)
+	{
+		float uTexture = u * static_cast<float>(texture.GetWidth());
+		float vTexture = v * static_cast<float>(texture.GetHeight());
+
+		int uIndex = static_cast<int>(uTexture);
+		int vIndex = static_cast<int>(vTexture);
+
+		float uRatio = uTexture - static_cast<float>(uIndex);
+		float vRatio = vTexture - static_cast<float>(vIndex);
+
+		float uOpposite = 1.0f - uRatio;
+		float vOpposite = 1.0f - vRatio;
+
+		X::Color a = texture.GetPixel(uIndex, vIndex) * uOpposite;
+		X::Color b = texture.GetPixel(uIndex + 1, vIndex) * uRatio;
+		X::Color c = texture.GetPixel(uIndex, vIndex + 1) * uOpposite;
+		X::Color d = texture.GetPixel(uIndex + 1, vIndex + 1) * uRatio;
+
+		return (a + b) * vOpposite + (c + d) * vRatio;
+	}
 }
 
 void Texture::Load(const std::string& fileName)
@@ -50,68 +72,68 @@ void Texture::Load(const std::string& fileName)
 		return;
 	}
 
-    BitmapFileHeader fileHeader;
-    BitmapInfoHeader fileInfo;
-    fread(&fileHeader, sizeof(fileHeader), 1, file);
-    fread(&fileInfo, sizeof(fileInfo), 1, file);
+	BitmapFileHeader fileHeader;
+	BitmapInfoHeader fileInfo;
+	fread(&fileHeader, sizeof(fileHeader), 1, file);
+	fread(&fileInfo, sizeof(fileInfo), 1, file);
 
-    if (fileInfo.bits != 24)
-    {
-        fclose(file);
-        return;
-    }
+	if (fileInfo.bits != 24)
+	{
+		fclose(file);
+		return;
+	}
 
-    mWidth = fileInfo.width;
-    mHeight = fileInfo.height;
-    mPixels = std::make_unique<X::Color[]>(mWidth * mHeight);
+	mWidth = fileInfo.width;
+	mHeight = fileInfo.height;
+	mPixels = std::make_unique<X::Color[]>(mWidth * mHeight);
 
-    fseek(file, fileHeader.offset, SEEK_SET);
+	fseek(file, fileHeader.offset, SEEK_SET);
 
-    uint32_t rowStride = mWidth * fileInfo.bits / 8;
-    uint32_t paddedStride = MakeStringAligned(rowStride, 4);
-    std::vector<uint8_t> paddingBytes(paddedStride - rowStride);
+	uint32_t rowStride = mWidth * fileInfo.bits / 8;
+	uint32_t paddedStride = MakeStringAligned(rowStride, 4);
+	std::vector<uint8_t> paddingBytes(paddedStride - rowStride);
 
-    for (int h = 0; h < mHeight; ++h)
-    {
-        for (int w = 0; w < mWidth; ++w)
-        {
-            uint8_t r, g, b;
-            fread(&b, sizeof(uint8_t), 1, file);
-            fread(&g, sizeof(uint8_t), 1, file);
-            fread(&r, sizeof(uint8_t), 1, file);
-            uint32_t index = w + ((mHeight - h - 1) * mWidth);
-            mPixels[index] = { r / 255.0f, g / 255.0f, b / 255.0f, 1.0f };
-        }
-        fread((char*)paddingBytes.data(), paddingBytes.size(), 1, file);
-    }
-    fclose(file);
+	for (int h = 0; h < mHeight; ++h)
+	{
+		for (int w = 0; w < mWidth; ++w)
+		{
+			uint8_t r, g, b;
+			fread(&b, sizeof(uint8_t), 1, file);
+			fread(&g, sizeof(uint8_t), 1, file);
+			fread(&r, sizeof(uint8_t), 1, file);
+			uint32_t index = w + ((mHeight - h - 1) * mWidth);
+			mPixels[index] = { r / 255.0f, g / 255.0f, b / 255.0f, 1.0f };
+		}
+		fread((char*)paddingBytes.data(), paddingBytes.size(), 1, file);
+	}
+	fclose(file);
 }
 
 const std::string& Texture::GetFileName() const
 {
-    return mFileName;
+	return mFileName;
 }
 
 X::Color Texture::GetPixel(int x, int y) const
 {
-    x = std::clamp(x, 0, mWidth - 1);
-    y = std::clamp(y, 0, mHeight - 1);
-    return mPixels[x + (y * mWidth)];
+	x = std::clamp(x, 0, mWidth - 1);
+	y = std::clamp(y, 0, mHeight - 1);
+	return mPixels[x + (y * mWidth)];
 }
 
 X::Color Texture::GetPixel(float u, float v) const
 {
-    int uIndex = static_cast<int>(u * (mWidth - 1) + 0.5f);
-    int vIndex = static_cast<int>(v * (mHeight - 1) + 0.5f);
-    return GetPixel(uIndex, vIndex);
+	int uIndex = static_cast<int>(u * (mWidth - 1) + 0.5f);
+	int vIndex = static_cast<int>(v * (mHeight - 1) + 0.5f);
+	return GetPixel(uIndex, vIndex);
 }
 
 int Texture::GetWidth() const
 {
-    return mWidth;
+	return mWidth;
 }
 
 int Texture::GetHeight() const
 {
-    return mHeight;
+	return mHeight;
 }
